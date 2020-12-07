97e911da5383311ae1ca2285843dcd0199165913
Juan Luis Pérez <juanluis.perez.cabrera@bbva.com>
2020-12-07 21:30:28 +0100

    ..

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 
 import MyBookShelf from '../components/MyBookShelf'
-import MyHeader from '../components/MyHeaders'
+import MyHeader from '../components/MyHeaders' 
 class Home extends Component {
 
   state = {
